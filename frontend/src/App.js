25aa73686142dd802bc32feb26b30001d3d1eabf
victorgois <victorgois18@gmail.com>
2019-06-13 00:53:11 -0300

    começando a desenvolver design da pagina de feed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Header from './components/Header';
+import Routes from './routes';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/pages/Feed.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Feed.css';
+
+class Feed extends Component{
+    render(){
+        return (
+            <h1>Feed</h1>
+        );
+    }
+}
+
+export default Feed;
